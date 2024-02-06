005f6c386c7075bfca1cd3cf7069288e809166fd
ArtemChuban <ArtemChuban@yandex.ru>
2024-02-06 13:39:07 +0200

    put_users_password endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -32,7 +32,6 @@ users_collection = database["users"]
 
 
 class User(BaseModel):
-    id: str = Field(default_factory=lambda: str(uuid.uuid4()))
     username: str
     password: str
     is_admin: bool = False
@@ -42,18 +41,27 @@ class User(BaseModel):
 def get_user_by_token(token: str) -> User:
     result = users_collection.find_one({"tokens": token})
     if result is None:
-        return None
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
     return User(**result)
+
+
+def get_user_by_username(username: str) -> User:
+    result = users_collection.find_one({"username": username})
+    if result is None:
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
+    return User(**result)
+
+
+def user_exist(username: str) -> bool:
+    return users_collection.find_one({"username": username}) is not None
 
 
 @app.post("/users/create")
 async def post_users_register(token: str, username: str) -> None:
     user = get_user_by_token(token)
-    if user is None:
-        raise HTTPException(status.HTTP_404_NOT_FOUND)
     if not user.is_admin:
         raise HTTPException(status.HTTP_403_FORBIDDEN)
-    if users_collection.find_one({"username": username}) is not None:
+    if user_exist(username):
         raise HTTPException(status.HTTP_400_BAD_REQUEST)
     new_user = User(username=username, password=username)
     users_collection.insert_one(new_user.model_dump())
@@ -61,10 +69,20 @@ async def post_users_register(token: str, username: str) -> None:
 
 @app.get("/users/token")
 async def get_users_token(username: str, password: str) -> str:
-    user = users_collection.find_one({"username": username, "password": password})
-    if user is None:
-        raise HTTPException(status.HTTP_404_NOT_FOUND)
-    user = User(**user)
+    user = get_user_by_username(username)
+    if user.password != password:
+        raise HTTPException(status.HTTP_403_FORBIDDEN)
     token = str(uuid.uuid4())
-    users_collection.update_one({"id": user.id}, {"$push": {"tokens": token}})
+    users_collection.update_one(
+        {"username": user.username}, {"$push": {"tokens": token}}
+    )
     return token
+
+
+@app.put("/users/password")
+async def put_users_password(token: str, username: str, new_password: str) -> None:
+    current_user = get_user_by_token(token)
+    user_to_update = get_user_by_username(username)
+    if not current_user.is_admin and current_user.username != user_to_update.username:
+        raise HTTPException(status.HTTP_403_FORBIDDEN)
+    users_collection.update_one({"username": username}, {"password": new_password})
