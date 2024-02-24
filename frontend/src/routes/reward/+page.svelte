9284bcea0da79da655d134bdcdcd3bb206e1a059
ArtemChuban <ArtemChuban@yandex.ru>
2024-02-24 14:24:26 +0200

    Chip and Autocomplete usage for reward page

--- frontend/src/routes/reward/+page.svelte
+++ frontend/src/routes/reward/+page.svelte
@@ -1,53 +1,61 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
-	import { getUser, reward } from '$lib/api';
+	import { getAllUsers, getUser, reward } from '$lib/api';
 	import FetchStatus from '$lib/FetchStatus.svelte';
-	import UserSelect from '$lib/UserSelect.svelte';
 	import { t } from '$lib/i18n';
 	import { session } from '$lib/user';
 	import { writable } from 'svelte/store';
-	import { fly } from 'svelte/transition';
-	import { getToastStore } from '@skeletonlabs/skeleton';
+	import {
+		Autocomplete,
+		InputChip,
+		getToastStore,
+		type AutocompleteOption
+	} from '@skeletonlabs/skeleton';
 	const toastStore = getToastStore();
 
-	let usernames = writable(new Set<string>());
-	let username = writable('');
 	let value = '';
 	let promise: Promise<void>;
-	let selectable: Array<string> = [];
+	let inputChip = '';
+	let inputChipList: Array<string> = [];
+	const options = writable<Array<AutocompleteOption<string>>>([]);
+	let inputChipComponent: InputChip;
+	let whitelist = writable<Array<string>>([]);
+
+	options.subscribe((options) => {
+		$whitelist = options.map((option) => option.value);
+	});
 
 	onMount(async () => {
 		session.subscribe(async (value) => {
 			if (value === null) return;
 			if (!(await getUser(value)).is_admin) goto('/');
 		});
+		getUsernames();
 	});
 
-	username.subscribe((value) => {
-		if (value === '') return;
-		$usernames.add($username);
-		$usernames = $usernames;
-		$username = '';
-		selectable.splice(selectable.indexOf(value), 1);
-	});
-
-	const deselect = (username: string) => {
-		$usernames.delete(username);
-		$usernames = $usernames;
-		selectable = [...selectable, username];
+	const getUsernames = async () => {
+		(await getAllUsers()).forEach((user) => {
+			$options.push({ label: user.username, value: user.username });
+		});
+		$options = $options;
 	};
 
 	const handleReward = async () => {
-		$usernames.forEach(async (username) => {
+		inputChipList.forEach(async (username) => {
 			if ($session === null) return;
 			await reward($session, username, +value);
 		});
 		toastStore.trigger({
-			message: `${Array.from($usernames)} rewarded for ${value} token${+value === 1 ? '' : 's'}`,
+			message: `${Array.from(inputChipList)} rewarded for ${value} token${+value === 1 ? '' : 's'}`,
 			background: 'variant-filled-success'
 		});
 		goto('/');
+	};
+
+	const onInputChipSelect = (event: CustomEvent) => {
+		inputChipComponent.addChip(event.detail.value);
+		inputChip = '';
 	};
 </script>
 
@@ -58,19 +66,24 @@
 <div class="w-1/2">
 	<h1 class="text-center mb-3 text-slate-100 font-bold text-xl">{$t('reward.title')}</h1>
 	<form on:submit|preventDefault={() => (promise = handleReward())} class="flex flex-col">
-		<p class="text-slate-100 text-sm">{$t('username')}</p>
-		<UserSelect bind:username={$username} bind:usernames={selectable} />
-		<div class="w-full flex flex-wrap mt-5">
-			{#each $usernames as username (username)}
-				<button
-					transition:fly={{ y: 50 }}
-					type="button"
-					on:click|preventDefault={() => deselect(username)}
-					class="bg-slate-800 rounded-md p-1 mr-2 mb-2">{username}</button
-				>
-			{/each}
+		<InputChip
+			bind:this={inputChipComponent}
+			bind:input={inputChip}
+			bind:value={inputChipList}
+			placeholder={'Search...'}
+			whitelist={$whitelist}
+			allowUpperCase
+			name="chips"
+		/>
+		<div class="card mt-5 max-h-48 overflow-y-auto" tabindex="-1">
+			<Autocomplete
+				bind:input={inputChip}
+				options={$options}
+				denylist={inputChipList}
+				on:selection={onInputChipSelect}
+			/>
 		</div>
-		<input bind:value type="number" class="input mb-5" placeholder={$t('value')} />
+		<input bind:value type="number" class="input my-5" placeholder={$t('value')} />
 		<button type="submit" class="btn variant-filled-surface text-sm">{$t('reward.button')}</button>
 	</form>
 </div>
