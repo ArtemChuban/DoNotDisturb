f98e8162510a88cbb2f639cf5f3e4236cdf2cdee
ArtemChuban <ArtemChuban@yandex.ru>
2024-03-14 09:09:34 +0200

    create team routes

--- backend/app/Controller.py
+++ backend/app/Controller.py
@@ -50,6 +50,24 @@ class Controller:
         )
         return count > 0
 
+    def team_name_exist(self, name: str) -> bool:
+        query = f"select count(*) as `count` from Teams where `name` = '{name}';"
+        count = (
+            self.__session_pool.retry_operation_sync(self.callee, query=query)[0]
+            .rows[0]
+            .count
+        )
+        return count > 0
+
+    def get_user_id_by_session(self, session: str) -> str:
+        query = f"select `user_id` from Sessions where `session` = '{session}';"
+        rows = self.__session_pool.retry_operation_sync(self.callee, query=query)[
+            0
+        ].rows
+        if len(rows) == 0:
+            raise HTTPException(status.HTTP_401_UNAUTHORIZED)
+        return rows[0].user_id
+
     def try_register(self, username: str, password: str) -> str:
         if self.username_exist(username):
             raise HTTPException(status.HTTP_409_CONFLICT)
@@ -66,6 +84,19 @@ class Controller:
         self.__session_pool.retry_operation_sync(self.callee, query=query)
         return user_session
 
+    def create_team(self, user_id: str, name: str) -> str:
+        if self.team_name_exist(name):
+            raise HTTPException(status.HTTP_409_CONFLICT)
+        team_id = str(uuid.uuid4())
+        query = f"insert into Teams (id, name) values ('{team_id}', '{name}');"
+        self.__session_pool.retry_operation_sync(self.callee, query=query)
+        return team_id
+
+    def add_member(self, user_id: str, team_id: str, is_admin: bool = False) -> None:
+        query = f"insert into Membership (user_id, team_id, is_admin, tokens) \
+                values ('{user_id}', '{team_id}', {is_admin}, 0);"
+        self.__session_pool.retry_operation_sync(self.callee, query=query)
+
     def try_login(self, username: str, password: str) -> str:
         query = f"select id, password from Users where `username` = '{username}';"
         users = self.__session_pool.retry_operation_sync(self.callee, query=query)[
@@ -78,3 +109,9 @@ class Controller:
         if not verify_password(password, hashed_password):
             raise HTTPException(status.HTTP_401_UNAUTHORIZED)
         return self.create_session(user.id)
+
+    def try_create_team(self, session: str, name: str) -> str:
+        user_id = self.get_user_id_by_session(session)
+        team_id = self.create_team(user_id, name)
+        self.add_member(user_id, team_id, is_admin=True)
+        return team_id
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 
 import uvicorn
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, Header
 from contextlib import asynccontextmanager
 from Controller import Controller
 import os
@@ -20,20 +20,27 @@ controller = Controller()
 
 @app.post("/users")
 async def post_users(
-    username: Annotated[str, Body()],
-    password: Annotated[str, Body()],
+    username: Annotated[str, Body(embed=True)],
+    password: Annotated[str, Body(embed=True)],
 ) -> str:
     return controller.try_register(username, password)
 
 
 @app.post("/users/session")
 async def post_users_session(
-    username: Annotated[str, Body()],
-    password: Annotated[str, Body()],
+    username: Annotated[str, Body(embed=True)],
+    password: Annotated[str, Body(embed=True)],
 ) -> str:
     return controller.try_login(username, password)
 
 
+@app.post("/teams")
+async def post_team(
+    session: Annotated[str, Header()],
+    name: Annotated[str, Body(embed=True)],
+) -> str:
+    return controller.try_create_team(session, name)
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", "8000")))
-    print("Test")
